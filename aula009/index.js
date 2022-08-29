b742be17f1a711ae0414bbefcb0a6e0bd62e62ce
Danilo Costa <danilopscosta@gmail.com>
2022-08-29 10:42:58 -0300

    criei um teste

---
+++ aula009/index.js
@@ -0,0 +1,8 @@
+
+
+window.document.getElementsByName('divi')[0].innerText = 'zoeira'
+
+window.document.getElementsByTagName('h1')[0].innerText = 'Danilo'
+
+window.document.getElementsByTagName('p')[0].innerText = 'zoeira'
+
