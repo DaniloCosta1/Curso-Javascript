ee6b0bf7c2c4c9401660f54db12af0cf2db3d152
Danilo Costa <danilopscosta@gmail.com>
2022-08-29 21:50:57 -0300

    criei ex013

---
+++ aula012/ex012.js
@@ -0,0 +1,18 @@
+var agora = new Date()
+
+var hora = agora.getHours()
+
+console.log(`Agora são exatamente ${hora} horas.`)
+
+if(hora < 12){
+      console.log('Bom dia!')
+}
+else if(hora <= 18){
+      console.log('Boa tarde!')
+}
+else if( hora > 18 && hora <23){
+      console.log('Boa noite!')
+}
+else if(hora > 00 && hora < 6){
+      console.log('boa madrugada')
+}
