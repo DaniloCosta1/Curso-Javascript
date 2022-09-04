e839e0468e9379a4633677d65f075f99863fb2f8
Danilo Costa <danilopscosta@gmail.com>
2022-09-04 10:46:46 -0300

    criei aula 14

--- aula013/ambiente.js
+++ aula013/ambiente.js
@@ -1,6 +1,15 @@
 var c = 1
-while (c < 10) {
+while (c < 5) {
       console.log('oi')
-      console.log('gggggu')
+      console.log('olá')
       c++
 }
+
+c2 = 0
+do {
+
+      console.log("danilo")
+      console.log("teste")
+      c2++
+
+} while(c2 < 5)
---
+++ aula014/ambiente.js
@@ -0,0 +1,5 @@
+var c = 0
+
+for(c = 0; c <= 10; c++){
+      console.log(c)
+}
